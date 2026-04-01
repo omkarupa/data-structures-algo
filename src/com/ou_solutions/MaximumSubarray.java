7a6923a298777b956bec6633c82d1d50efb3509b
Omkar Upadhyay <omkarmh009@gmail.com>
2026-04-01 18:24:14 +0530

    Find max sum of subarray

---
+++ src/com/ou_solutions/MaximumSubarray.java
@@ -0,0 +1,30 @@
+package com.ou_solutions;
+
+public class MaximumSubarray {
+
+	public static void main(String[] args) {
+		
+		int [] nums = {-2,1,-3,4,-1,2,1,-5,4};
+		
+		System.out.println(maxSumSubArray(nums));
+		
+	}
+	
+	public static int maxSumSubArray(int [] nums)
+	{
+		
+		int currentSum = nums[0];
+		int maxSum = nums[0];
+		
+		for(int i = 1;i<nums.length;i++)
+		{
+			
+			currentSum = Math.max(nums[i],currentSum + nums[i]);
+			maxSum = Math.max(maxSum,currentSum);
+
+		}
+		
+		return maxSum;
+	}
+
+}
